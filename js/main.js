18535794aa6d30203a373def305541123341ce4c
Pauline <makarova.pv@edu.spbstu.ru>
2023-04-18 10:53:29 +0300

    7-2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { createPublication } from './data.js';
 import { drawPictures } from './pictures.js';
 
-const publ = createPublication();
-drawPictures(publ);
+drawPictures(createPublication());
+//Задание 7 часть 2
